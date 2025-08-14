b21badd6fc43ea6adefaa0964c10112399410a78
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2025-08-14 06:23:32 +0000

    Added Comment

--- PYTHON_PROJECT/BRAND_NAME_GENERATOR.py
+++ PYTHON_PROJECT/BRAND_NAME_GENERATOR.py
@@ -1,3 +1,4 @@
+#DAY - 1 PYTHON LEARNING PROJECT
 print("Welcome to the Brand Name Generator.")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
