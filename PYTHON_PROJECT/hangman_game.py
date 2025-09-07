4c86738ffffd9cee69883694454618087c336f3f
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2025-09-07 08:09:45 +0000

    Step-2 edit

--- PYTHON_PROJECT/hangman_game.py
+++ PYTHON_PROJECT/hangman_game.py
@@ -1,12 +1,16 @@
 import random
 word_list = ["python", "java", "kotlin", "javascript"]
+
 chosen_word = random.choice(word_list)
 print(chosen_word)
+
 placeholder = ""
 for i in range(len(chosen_word)):
     placeholder += "_"
 print(placeholder)
+
 guess = input("Guess a letter otherwise you will be hanged: ").lower()
+
 display = ""
 for i in range(len(chosen_word)):
     if chosen_word[i] == guess:
@@ -14,6 +18,7 @@ for i in range(len(chosen_word)):
     else:
         display += "_"
 print(display)
+
 for i in chosen_word:
   for j in range(len(guess)):
         if i == guess[j]:
