6df3bdcf1252ff02fd25c5bc896a826689bfbc8b
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2025-09-13 14:51:41 +0000

    Step -3 Changes

--- PYTHON_PROJECT/hangman_game.py
+++ PYTHON_PROJECT/hangman_game.py
@@ -9,15 +9,24 @@ for i in range(len(chosen_word)):
     placeholder += "_"
 print(placeholder)
 
-guess = input("Guess a letter otherwise you will be hanged: ").lower()
 
-display = ""
-for i in range(len(chosen_word)):
-    if chosen_word[i] == guess:
-        display += chosen_word[i]
-    else:
-        display += "_"
-print(display)
+game_over = False
+list_correct = []
+while not game_over:
+    display = ""
+    guess = input("Guess a letter otherwise you will be hanged: ").lower()
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            display += chosen_word[i]
+            list_correct.append(guess)
+        elif chosen_word[i] in list_correct:
+             display += chosen_word[i]
+        else:
+            display += "_"
+    print(display)
+    if "_" not in display:
+        print("You won the game")
+        game_over = True
 
 for i in chosen_word:
   for j in range(len(guess)):
