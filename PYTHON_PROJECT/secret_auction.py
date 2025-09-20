4f8ed8fb59e667bc4627a341ba1a29dadf2e1ea8
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2025-09-20 11:27:01 +0000

    New_edit

--- PYTHON_PROJECT/secret_auction.py
+++ PYTHON_PROJECT/secret_auction.py
@@ -1,5 +1,16 @@
 import ascii_art
 
+
+def highest_bidder(bidding_list):
+    max_bid = 0
+    winner = ''
+    for i in bidding_list:
+        bidding_Amount = bidding_list[i]
+        if bidding_Amount > max_bid:
+            max_bid = bidding_Amount
+            winner = i
+    print(f"The Highest Bidder is {winner} with the Bid of {max_bid}")
+
 print(ascii_art.auction_logo)
 
 bidders = {}
@@ -8,12 +19,17 @@ print("Welcome to the Secret Auction Programm.")
 
 stop = False
 while not stop:
-    name = input("What is your name?:  ")
+    name = input("\nWhat is your name?:  ")
     bid = int(input("What is your Bid: $"))
     bidders[name] =  bid
-    print(bidders)
-    response = print(input("Are there any more bidders? : Type 'yes' or 'no'.\n").lower())
+    response = input("Are there any more bidders? : Type 'yes' or 'no'.\n").lower()
     if response == 'yes':
         stop = False
+        print("\n"*1000)
     else:
         stop = True
+        print("\n"*1000)
+        highest_bidder(bidders)
+
+
+    
