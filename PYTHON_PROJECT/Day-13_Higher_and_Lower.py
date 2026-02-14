dd4a29a46999201f45000e01c41df21e6c75f313
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2026-02-14 14:17:25 +0000

    Updating_Day_13_Code

--- PYTHON_PROJECT/Day-13_Higher_and_Lower.py
+++ PYTHON_PROJECT/Day-13_Higher_and_Lower.py
@@ -1,6 +1,10 @@
 import ascii_art
 import game_data
 
-print(ascii_art.higher_lower_logo)
+#Function to generate 2 random data from the game_data list
 
-print(game_data.data[0]['name'])
+print(ascii_art.higher_lower_logo)
+print(f"Compare A: {game_data.data[0]['name']}, a {game_data.data[0]['description']} from {game_data.data[0]['country']}")
+print(ascii_art.higher_lower_vs_logo)
+print(f"Compare B: {game_data.data[0]['name']}, a {game_data.data[0]['description']} from {game_data.data[0]['country']}")
+user_input = input("Who has more followers? Type 'A' or 'B': ").upper()
