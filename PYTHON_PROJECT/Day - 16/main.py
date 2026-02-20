ca4b5b56ce4a5545e90344c9d352d3412e87b347
Barathiselvan <barathiselvanofficial@gmail.com>
2026-02-20 13:12:57 +0000

    update

--- PYTHON_PROJECT/Day - 16/main.py
+++ PYTHON_PROJECT/Day - 16/main.py
@@ -4,19 +4,21 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
-menu_items = MenuItem('name','ingredients' ,'cost')
 menu = Menu()
-user_input = input('What would you like? (espresso/latte/cappuccino): ')
-coffee_maker.is_resource_sufficient(menu_items.name)
-# power_on = True
-# while power_on:
-#     user_input = input('What would you like? (espresso/latte/cappuccino): ')
-#     if user_input == 'report':
-#         coffee_maker.report()
-#         money_machine.report()
 
-#     if coffee_maker.is_resource_sufficient(menu_items.user_input):
+power_on = True
+while power_on:
+    options = menu.get_items()
+    user_input = input(f'What would you like? ({options}): ')
+    if user_input == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif user_input == 'off':
+        print("The Machine is under Maintanence!")
+        power_on = False
+    else:
+        drink = menu.find_drink(user_input)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+        
 
-#     elif user_input == 'off':
-#         print("The Machine is under Maintanence!")
-#         power_on = False
