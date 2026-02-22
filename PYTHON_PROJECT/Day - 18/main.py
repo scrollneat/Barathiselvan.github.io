da5b866042880b7246ef05c3c0fb9fda814e6c83
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2026-02-22 22:40:34 +0530

    Refactor turtle drawing with random colors
    Refactored turtle graphics code to use random colors and improved structure.

--- PYTHON_PROJECT/Day - 18/main.py
+++ PYTHON_PROJECT/Day - 18/main.py
@@ -1,5 +1,6 @@
-from turtle import *
+import turtle
 import random
+
 
 def draw_shape(sides):
   angle = 360/sides
@@ -7,8 +8,16 @@ def draw_shape(sides):
     vis.forward(50)
     vis.right(angle)
 
-vis = Turtle()
+# turtle.colormode(255)
+def random_color():
+  r = random.randint(0,255)
+  g = random.randint(0,255)
+  b = random.randint(0,255)
+  ran_color = (r,g,b)
+  return ran_color
+vis = turtle.Turtle()
 vis.shape("classic")
+vis.speed(0)
 
 for i in range (4):
   vis.forward(100)
@@ -28,7 +37,28 @@ vis.setposition(-50,200)
 vis.clear()
 
 vis.pendown()
-colours = ['light gray', 'royal blue', 'gold','red','pink','cyan','aquamarine','burlywood', 'dark cyan', 'spring green', 'orange red', 'violet', 'dark olive green']
-for i in range (3,20):
-  vis.pencolor(random.choice(colours))
+# colours = ['light gray', 'royal blue', 'gold','red','pink','cyan','aquamarine','burlywood', 'dark cyan', 'spring green', 'orange red', 'violet', 'dark olive green']
+for i in range (3,12):
+  vis.pencolor(random_color())
   draw_shape(i)
+vis.clear()
+
+vis.pendown()
+vis.pensize(10)
+direction = [0, 90, 180, 270, 360]
+vis.setposition(25,0)
+vis.clear()
+for i in range (100):
+  vis.forward(20)
+  vis.pencolor(random_color())
+  vis.left(random.choice(direction))
+vis.clear()
+
+for i in range (20):
+  vis.pendown()
+  vis.circle(5)
+  vis.pencolor(random_color())
+  vis.penup()
+  vis.forward(20)
+  turtle.tilt(45)
+  vis.pendown()
