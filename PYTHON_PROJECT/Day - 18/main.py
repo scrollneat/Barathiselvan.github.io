5d179ed258e5896da04f541d87a65fec3a25ff9d
Barathiselvan <barathiselvanofficial@gmail.com>
2026-02-22 15:16:22 +0000

    update

--- PYTHON_PROJECT/Day - 18/main.py
+++ PYTHON_PROJECT/Day - 18/main.py
@@ -1,7 +1,34 @@
 from turtle import *
+import random
 
+def draw_shape(sides):
+  angle = 360/sides
+  for sides in range (sides):
+    vis.forward(50)
+    vis.right(angle)
 
-# mosqu_the_turtle = Turtle()
+vis = Turtle()
+vis.shape("classic")
 
-# screen = Screen()
- 
+for i in range (4):
+  vis.forward(100)
+  vis.left(90)
+
+vis.clear()
+
+for i in range (10):
+  vis.pendown()
+  vis.forward(10)
+  vis.penup()
+  vis.forward(10)
+  
+vis.clear()
+
+vis.setposition(-50,200)
+vis.clear()
+
+vis.pendown()
+colours = ['light gray', 'royal blue', 'gold','red','pink','cyan','aquamarine','burlywood', 'dark cyan', 'spring green', 'orange red', 'violet', 'dark olive green']
+for i in range (3,20):
+  vis.pencolor(random.choice(colours))
+  draw_shape(i)
