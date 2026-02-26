9dc5e80a131e63a26df56b5ea4f7c9146a0db300
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2026-02-26 21:52:09 +0530

    Update main.py

--- PYTHON_PROJECT/Day - 19/main.py
+++ PYTHON_PROJECT/Day - 19/main.py
@@ -1,1 +1,35 @@
+#--------------------Etch-a-Sketch App------------------------------------
+
+from turtle import Turtle, Screen
+
+vis = Turtle()
+screen = Screen()
+
+def move_forward():
+	vis.setheading(0)
+	vis.forward(50)
+  
+def move_backwards():
+	vis.setheading(180)
+	vis.forward(50)
+
+def move_counter_clockwise():
+	vis.setheading(30)
+	vis.circle(100, 40)
+  
+def move_clockwise():
+	vis.setheading(180)
+	vis.circle(50, 10)
+
+def clean():
+  vis.clear()
+  vis.home()
+  
+screen.listen()
+screen.onkey(move_forward , 'w')
+screen.onkey(move_backwards , 's')
+screen.onkey(move_counter_clockwise, 'a')
+screen.onkey(move_clockwise, 'd')
+screen.onkey(clean, 'c')
+
 
