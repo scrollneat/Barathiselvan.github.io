e6c43e2878bca1d00868e06edd90bcea6afe88ad
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2026-02-27 08:58:27 +0530

    Update main.py

--- PYTHON_PROJECT/Day - 21/main.py
+++ PYTHON_PROJECT/Day - 21/main.py
@@ -1,1 +1,18 @@
+#--------------------Snake game------------------------------------
 
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+#screen.title("Snake Game")
+positions = []
+
+for i in range(0,41,20):
+	turtle_1 = Turtle()
+	turtle_1.color('white')
+	turtle_1.shape('square')
+	turtle_1.goto(-i, 0)
+  
+
+screen.exitonclick()
