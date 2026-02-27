a3e813375b8abe99a47e6af802de5cbd17b4e71c
Barathiselvan <85095295+scrollneat@users.noreply.github.com>
2026-02-27 21:51:20 +0530

    Update main.py

--- PYTHON_PROJECT/Day - 21/main.py
+++ PYTHON_PROJECT/Day - 21/main.py
@@ -1,21 +1,27 @@
 #--------------------Snake game------------------------------------
 
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
 screen.bgcolor("black")
 #screen.title("Snake Game")
+screen.tracer(0)
 positions = []
-
 for i in range(0,41,20):
 	turtle_1 = Turtle()
+	turtle_1.penup()
 	turtle_1.color('white')
 	turtle_1.shape('square')
 	turtle_1.goto(-i, 0)
-  
+	positions.append(turtle_1)
+
 game_over = True
 while game_over:
+	time.sleep(0.1)
+	screen.update()
 	for part in positions:
 		part.forward(20)
+		
 screen.exitonclick()
