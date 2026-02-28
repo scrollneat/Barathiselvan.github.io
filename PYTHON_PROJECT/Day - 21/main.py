efa507899f19bea1c3284e886faf762800c5d00e
Barathi Selvan <barathiselvanofficial@gmail.com>
2026-02-28 04:12:28 +0000

    updated code

--- PYTHON_PROJECT/Day - 21/main.py
+++ PYTHON_PROJECT/Day - 21/main.py
@@ -1,30 +1,17 @@
 #--------------------Snake game------------------------------------
-
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
+from snake import Snake
 screen = Screen()
 screen.setup(width = 600, height = 600)
 screen.bgcolor("black")
 #screen.title("Snake Game")
 screen.tracer(0)
-positions = []
-for i in range(0,41,20):
-	turtle_1 = Turtle()
-	turtle_1.penup()
-	turtle_1.color('white')
-	turtle_1.shape('square')
-	turtle_1.goto(-i, 0)
-	positions.append(turtle_1)
-
+snake = Snake()
 game_over = True
 while game_over:
 	time.sleep(0.1)
 	screen.update()
-	
-	for pos_num in range(len(positions) -1, 0, -1):
-		new_x = positions[pos_num - 1].xcor()
-		new_y = positions[pos_num - 1].ycor()
-		positions[pos_num].goto(new_x,new_y)
-	positions[0].forward(20)
+	snake.move()
     
 screen.exitonclick()
--- PYTHON_PROJECT/Day - 21/snake.py
+++ PYTHON_PROJECT/Day - 21/snake.py
@@ -10,6 +10,11 @@ class Snake:
             self.segment.goto(-i,0)
             self.positions.append(segment)
 
-
-        
+    def move(self):
+        for pos_num in range(len(self.positions) -1, 0, -1):
+            new_x = self.positions[pos_num - 1].xcor()
+            new_y = self.positions[pos_num - 1].ycor()
+            self.positions[pos_num].goto(new_x,new_y)
+            self.positions[0].forward(20)
+            
 
