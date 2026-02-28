47efa2e4fc0056de96ed4a004b6d57f64cecc014
Barathi Selvan <barathiselvanofficial@gmail.com>
2026-02-28 04:04:56 +0000

    update

---
+++ PYTHON_PROJECT/Day - 21/snake.py
@@ -0,0 +1,15 @@
+import turtle
+class Snake:
+    def __int__(self):
+        self.segment = Turtle()
+        self.positions = []
+        for i in range(0, 41, 20):
+            self.segment.penup()
+            self.segment.color('white')
+            self.segment.shape('square')
+            self.segment.goto(-i,0)
+            self.positions.append(segment)
+
+
+        
+
