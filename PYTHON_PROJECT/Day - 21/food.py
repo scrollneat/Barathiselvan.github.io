7be57fa164ccbc1245d93cd2f452feff0cec22f7
Barathiselvan <barathiselvanofficial@gmail.com>
2026-02-28 11:04:55 +0000

    updated the code

--- PYTHON_PROJECT/Day - 21/food.py
+++ PYTHON_PROJECT/Day - 21/food.py
@@ -8,9 +8,7 @@ class Food(Turtle):
         self.color("red")
         self.penup()
         self.shapesize(0.5,0.5)
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
-        self.goto(random_x, random_y)
+        self.refresh()
 
     def refresh(self):
         random_x = random.randint(-280, 280)
