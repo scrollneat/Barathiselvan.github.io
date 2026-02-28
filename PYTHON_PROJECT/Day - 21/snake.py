d59c38b2b9d79cc577e9f3b5021fda6d13351b6c
Barathi Selvan <barathiselvanofficial@gmail.com>
2026-02-28 04:41:20 +0000

    oops update

--- PYTHON_PROJECT/Day - 21/snake.py
+++ PYTHON_PROJECT/Day - 21/snake.py
@@ -1,20 +1,18 @@
 import turtle
 class Snake:
-    def __int__(self):
-        self.segment = Turtle()
+    def __init__(self):
         self.positions = []
         for i in range(0, 41, 20):
+            self.segment = turtle.Turtle()
             self.segment.penup()
             self.segment.color('white')
             self.segment.shape('square')
             self.segment.goto(-i,0)
-            self.positions.append(segment)
+            self.positions.append(self.segment)
 
     def move(self):
         for pos_num in range(len(self.positions) -1, 0, -1):
             new_x = self.positions[pos_num - 1].xcor()
             new_y = self.positions[pos_num - 1].ycor()
             self.positions[pos_num].goto(new_x,new_y)
-            self.positions[0].forward(20)
-            
-
+        self.positions[0].forward(20)
