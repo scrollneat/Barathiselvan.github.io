1bccd75a08129d319a4fe5b63ec4a04c868685af
Barathiselvan <barathiselvanofficial@gmail.com>
2026-03-07 11:23:05 +0530

    updated the code

--- PYTHON_PROJECT/Day - 23/main.py
+++ PYTHON_PROJECT/Day - 23/main.py
@@ -5,9 +5,12 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-
 screen.setup(width = 600, height = 600)
 screen.tracer(0)
+player = Player()
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
 
 game_on = True
 while game_on:
--- PYTHON_PROJECT/Day - 23/player.py
+++ PYTHON_PROJECT/Day - 23/player.py
@@ -1,7 +1,16 @@
 from turtle import Turtle
-STARTING_POSITION = (0,280)
+STARTING_POSITION = (0,-280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
