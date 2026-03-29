8eb9360c7e510175caa082272cbea5fd28dbbe30
Barathiselvan <barathiselvanofficial@gmail.com>
2026-03-29 02:21:01 +0000

    Updated the message

--- PYTHON_PROJECT/Day - 24/main.py
+++ PYTHON_PROJECT/Day - 24/main.py
@@ -1,1 +1,12 @@
+with open('PYTHON_PROJECT/Day - 24/Input/Letters/starting_letters.txt', 'r') as let:
+    letter = let.read()
+    print(letter)
+
+with open('PYTHON_PROJECT/Day - 24/Input/Names/invited_names.txt', 'r') as names:
+    invited_names = names.readlines()
+
+for i in invited_names:
+    new_invited_names = i.strip("\n")
+    with open(f"PYTHON_PROJECT/Day - 24/Output/ReadyToSend/{new_invited_names}.txt", 'w') as names:
+        names.write(letter.replace('[name]', new_invited_names))
 
