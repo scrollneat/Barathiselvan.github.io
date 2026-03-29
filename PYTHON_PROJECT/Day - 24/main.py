0f1b3b6ee4cd05fec1292be0ed3e27e0fabcdf68
Barathiselvan <barathiselvanofficial@gmail.com>
2026-03-29 02:25:10 +0000

    update

--- PYTHON_PROJECT/Day - 24/main.py
+++ PYTHON_PROJECT/Day - 24/main.py
@@ -7,6 +7,6 @@ with open('PYTHON_PROJECT/Day - 24/Input/Names/invited_names.txt', 'r') as names
 
 for i in invited_names:
     new_invited_names = i.strip("\n")
-    with open(f"PYTHON_PROJECT/Day - 24/Output/ReadyToSend/{new_invited_names}.txt", 'w') as names:
+    with open(f"PYTHON_PROJECT/Day - 24/Output/ReadyToSend/{new_invited_names}.docx", 'w') as names:
         names.write(letter.replace('[name]', new_invited_names))
 
