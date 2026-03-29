7b65fae4ef450ef91a53769f037777d68dbcd9ba
Barathiselvan <barathiselvanofficial@gmail.com>
2026-03-29 13:45:16 +0000

    update

--- PYTHON_PROJECT/Day - 25/main.py
+++ PYTHON_PROJECT/Day - 25/main.py
@@ -10,5 +10,36 @@
 # To get one column values above lines of code needs to be written. that is why we are going to use Pandas library
 
 import pandas as pd
+import numpy as np
 data = pd.read_csv("PYTHON_PROJECT/Day - 25/weather_data.csv")
+print(data)
+
+new_data = data.to_dict()
+print(new_data)
+
+temp_list = data['temp'].to_list()
+print(temp_list)
+
 print(data['temp'])
+print(data['temp'].mean())
+print(data['temp'].max())
+
+#Get Data from Column
+print(data['condition'])
+print(data.condition)
+# both are same above
+
+
+#to get column
+new = data[data.day == 'Monday']
+print(new)
+
+#row of data which has highest temperature
+hig_temp = data[data.temp == data.temp.max()]
+print(hig_temp)
+
+#to access specific column in a specific table
+monday = data[data.day == 'Monday']
+print(monday.condition)
+fah_temp= monday.temp[0]
+print((fah_temp*1.8)+32)
